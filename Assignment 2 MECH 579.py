a1a8f65e29b28e5622b5ed69bf6dd9900b1dc32e
Patrice Légaré <legarepatrice@yahoo.ca>
2025-10-09 17:27:38 -0400

    first commit

--- Assignment 2 MECH 579.py
+++ Assignment 2 MECH 579.py
@@ -1,2 +1,25 @@
 import numpy as np
 import matplotlib as plot
+
+#Rosenbrock Function
+def f(x,y):
+  return (1-x)**2+100*(y-x**2)**2
+
+
+#Steepest Descent
+#General form: xk+1=xk+ alpha*p
+
+#Initialize
+x=np.array([1.2],[1])
+alpha=1
+rho=
+
+#Gradient of f(x,y)
+def grad_f(x,y):
+  return np.array([2*x-2-400*x*y+400*x**3],[200*y-200*x**2])
+
+#Search direction p
+p=-1*grad_f(x,y)
+
+#Steph length alpha (Using simple backtracking line search) 
+#Start with alpha=1 and decrease
